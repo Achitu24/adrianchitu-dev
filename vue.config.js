aca0b3ef662cde99925f68cea18ffada91ddf0ae
achitu24 <adrian.chitu7@gmail.com>
2020-07-02 01:40:02 +0100

    bug

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,12 @@
-const GoogleFontsPlugin = require('google-fonts-plugin')
+// const GoogleFontsPlugin = require('google-fonts-plugin')
 
 module.exports = {
-  lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/adrianchitu-dev' : '/',
-  configureWebpack: {
-    plugins: [
-      new GoogleFontsPlugin('./src/fonts.json')
-    ]
-  }
+  lintOnSave: false
 }
+//   publicPath: process.env.NODE_ENV === 'production' ? '/adrianchitu-dev' : '/',
+//   configureWebpack: {
+//     plugins: [
+//       new GoogleFontsPlugin('./src/fonts.json')
+//     ]
+//   }
+// }
